7d193c77161a90d82ccb731eb30559152cba1db2
Wolker Windows note pessoal <contato@wolker.com.br>
2019-10-06 19:55:18 -0300

    Added header and logout

--- src/App.js
+++ src/App.js
@@ -7,25 +7,62 @@ import Vote from "./Vote";
 import Login from "./Login";
 import Results from "./Results";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  Button,
+  FormControl
+} from "react-bootstrap";
 
 function App() {
+  const logout = () => {
+    localStorage.removeItem("userId");
+    alert("Saiu!");
+  };
+
   return (
     <div className="App">
-      <h2>Desafio Técnico</h2>
-      <BrowserRouter>
-        <div>
-          <Route path="/cadastrar-pauta" exact component={CadastrarPauta} />
-        </div>
-        <div>
-          <Route path="/votar" exact component={Vote} />
-        </div>
-        <div>
-          <Route path="/" exact component={Login} />
-        </div>
-        <div>
-          <Route path="/resultados" exact component={Results} />
-        </div>
-      </BrowserRouter>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Desafio Técnico</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Entrar</Nav.Link>
+            <Nav.Link href="/cadastrar-pauta">Cadastrar Pauta</Nav.Link>
+            <Nav.Link href="/votar">Votar</Nav.Link>
+            <Nav.Link href="/resultados">Resultados</Nav.Link>
+            <Nav.Link href="/" onClick={() => logout()}>
+              Sair
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      {localStorage.getItem("userId") ? (
+        <BrowserRouter>
+          <div>
+            <Route path="/cadastrar-pauta" exact component={CadastrarPauta} />
+          </div>
+          <div>
+            <Route path="/votar" exact component={Vote} />
+          </div>
+          <div>
+            <Route path="/" exact component={Login} />
+          </div>
+          <div>
+            <Route path="/resultados" exact component={Results} />
+          </div>
+        </BrowserRouter>
+      ) : (
+        <BrowserRouter>
+          <div>
+            <Route path="/" component={Login} />
+          </div>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
